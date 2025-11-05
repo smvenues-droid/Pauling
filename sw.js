6e13d32c436fae903a48adb52a3f36e959ef9777
smvenues-droid <smvenues@gmail.com>
2025-11-05 00:54:45 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'paylog-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match(e.request).then(res => res || caches.match('./')))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(match => match || fetch(e.request))
+  );
+});
